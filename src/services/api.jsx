0a5019b5bfad691deeef2d1dd239b9a8350e322e
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2024-01-31 22:37:04 -0300

    :boom: rota backend render

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://notes-api-hvbp.onrender.com',
 })
 
 // https://notes-api-hvbp.onrender.com - em produção
